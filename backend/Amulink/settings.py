ed3a98bdd652fb251d6d448ae7e641c009e65689
Vishnu <turrillasgabriel@gmail.com>
2020-08-07 00:34:22 -0300

    configuraciones y merge con la BD postgres

--- backend/Amulink/settings.py
+++ backend/Amulink/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Amulink.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'amulink',
+        'USER': 'postgres',
+        'PASSWORD': 'Megagenial4233',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
