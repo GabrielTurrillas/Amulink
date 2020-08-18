e462798a4398a8b790a76bb48aa09eb6ecf65065
Vishnu <turrillasgabriel@gmail.com>
2020-08-18 07:14:44 -0300

    Creacion APIREST de modelo Paciente

--- backend/apps/paciente/urls.py
+++ backend/apps/paciente/urls.py
@@ -1,5 +1,7 @@
 from django .urls import path
-from .views import ListingsView, ListingsView, SearchView
+from .views import PacienteListCreateView, PacienteView
 
 urlpatterns = [
+    path('pacientes/', PacienteListCreateView.as_view()),
+    path('pacientes/<int:pk>', PacienteView.as_view()),
 ]
--- backend/apps/paciente/views.py
+++ backend/apps/paciente/views.py
@@ -1,3 +1,18 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework import permissions
+from .models import Paciente
+from .serializers import PacienteSerializer
+
+class PacienteListCreateView(ListCreateAPIView):
+    permission_classes = (permissions.AllowAny, )
+    queryset = Paciente.objects.all()
+    serializer_class = PacienteSerializer
+    pagination_class = None 
 
 
-# Create your views here.
+class PacienteView(RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.AllowAny, )
+    queryset = Paciente.objects.all()
+    serializer_class = PacienteSerializer
+
+
