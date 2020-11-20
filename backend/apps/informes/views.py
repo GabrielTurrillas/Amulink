f0fc6b6deac0e80af1d9ef9de9608a68c07f1ec5
Vishnu <turrillasgabriel@gmail.com>
2020-11-19 22:58:12 -0300

    requerimiento de informes escritos con comentarios en views

--- backend/apps/informes/views.py
+++ backend/apps/informes/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
-# Create your views here.
+""" 
+pagina resumen mensual (sitio terapeuta)
+    numero de horas atendidas(preguntar si 1 sesion=1hora)
+    pagos pendientes amulen(historico)(modalidad)
+    cantidad de pacientes totales
+"""
+
+"""
+ area comercial
+"""
+
+""" 
+area comercial (admin)
+    registro de ventas mensuales
+    cantidad de pacientes atendidos por mes
+    registro de ventas mensuales de cada terapeuta por tipo de terapia
+"""
+
+"""
+finansas (admin)
+    pagos de equipo interno a centro
+    pagos de equipo externo a centro
+    pago de derivaciones equipo interno
+    pago de derivaciones equipo externo
+    pago sesiones practicantes
+    atrasos
+"""
+
+"""
+area de operaciones
+    registro de cupos por terapeuta
+    panel de derivacion
+    pago y confirmacion de pago (terapeuta y administrador)
+    frecuencia sesion 
+"""
